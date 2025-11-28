632419b74dd3dedfb38bfbd13460a787d8c19d96
Tomassalda04 <164944852+Tomassalda04@users.noreply.github.com>
2025-11-28 14:52:01 -0500

    AvancesNivel1

--- CarreraEspacial/nivel.cpp
+++ CarreraEspacial/nivel.cpp
@@ -1,10 +1,15 @@
 // nivel.cpp
 #include "nivel.h"
+#include "enemigo.h"
 #include <QGraphicsPixmapItem>
+#include "obstaculo.h"
+#include <QTimer>
+#include <QDebug>
+
 
 Nivel::Nivel(int numeroNivel, QObject *parent)
-    : QGraphicsScene(parent),
-    m_numeroNivel(numeroNivel)
+    : QGraphicsScene(parent)
+    , m_numeroNivel(numeroNivel)
 {
     setSceneRect(0, 0, 1024, 572);
 
@@ -24,12 +29,60 @@ Nivel::Nivel(int numeroNivel, QObject *parent)
     }
 }
 
+<<<<<<< Updated upstream
 void Nivel::crearNivel1()
 {
     QPixmap fondo(":/Sprites/SpritesNivel1/FondoNivel1.png");
     addPixmap(fondo)->setPos(0, 0);
 
+=======
+void Nivel::crearNivel1(){
+
+    QPixmap fondo(":/Sprites/SpritesNivel1/fondo1.jpg");
+
+    setSceneRect(0, 0, VIEW_W, VIEW_H);
+
+    QPixmap fondoEscalado = fondo.scaled(
+        VIEW_W, VIEW_H,
+        Qt::IgnoreAspectRatio,
+        Qt::SmoothTransformation
+        );
+
+    addPixmap(fondoEscalado)->setPos(0, 0);
+
+    Obstaculo *obs1 = new Obstaculo(":/Sprites/SpritesNivel1/Obstaculos/Obs1.png");
+    Obstaculo *obs2 = new Obstaculo(":/Sprites/SpritesNivel1/Obstaculos/Obs2.png");
+    Obstaculo *obs3 = new Obstaculo(":/Sprites/SpritesNivel1/Obstaculos/Obs3.png");
+    Obstaculo *obs4 = new Obstaculo(":/Sprites/SpritesNivel1/Obstaculos/Obs4.png");
+    Obstaculo *obs5 = new Obstaculo(":/Sprites/SpritesNivel1/Obstaculos/Obs5.png");
+    Obstaculo *obs6 = new Obstaculo(":/Sprites/SpritesNivel1/Obstaculos/Obs6.png");
+    Obstaculo *obs7 = new Obstaculo(":/Sprites/SpritesNivel1/Obstaculos/Obs7.png");
+
+    obs1->setPos(300, 380);
+    obs2->setPos(600, 380);
+    obs3->setPos(900, 2);
+    obs4->setPos(550,350);
+    obs5->setPos(450,2);
+    obs6->setPos(150,35);
+    obs7->setPos(50,450);
+
+    addItem(obs1);
+    addItem(obs2);
+    addItem(obs3);
+    addItem(obs4);
+
+    m_personajePrueba = new Enemigo();
+    addItem(m_personajePrueba);
+
+    QPixmap sprite(":/Sprites/SpritesNivel1/Sprites_mov_soldados_enemigos/mov_abajo/mov1.png");
+    m_personajePrueba->setPixmap(sprite);
+
+    m_personajePrueba->setPos(100, 400);
+
+
+>>>>>>> Stashed changes
 }
+
 
 void Nivel::crearNivel2()
 {
---
+++ CarreraEspacial/personaje.cpp
@@ -0,0 +1,38 @@
+#include "personaje.h"
+
+Personaje::Personaje(QGraphicsItem *parent)
+    : QObject()
+    , QGraphicsPixmapItem(parent)
+    , m_velocidad(0.0, 0.0)
+    , m_aceleracion(0.0, 0.0)
+{
+}
+
+void Personaje::setVelocidadInicial(const QPointF &v0)
+{
+    m_velocidad = v0;
+}
+
+void Personaje::setAceleracion(const QPointF &a)
+{
+    m_aceleracion = a;
+}
+
+void Personaje::actualizarMovimiento(qreal dt)
+{
+    QPointF p = pos();
+
+    QPointF desplazamiento(
+        m_velocidad.x() * dt + 0.5 * m_aceleracion.x() * dt * dt,
+        m_velocidad.y() * dt + 0.5 * m_aceleracion.y() * dt * dt
+        );
+
+    p += desplazamiento;
+
+    // Actualizar velocidad: v = v + a*dt
+    m_velocidad.setX(m_velocidad.x() + m_aceleracion.x() * dt);
+    m_velocidad.setY(m_velocidad.y() + m_aceleracion.y() * dt);
+
+    // Aplicar nueva posición
+    setPos(p);
+}
