b291535fb17a303498a368cb1d2a8b7c0f2c4f1a
Tomassalda04 <164944852+Tomassalda04@users.noreply.github.com>
2025-12-06 12:43:39 -0500

    Nivel 3 70%

--- CarreraEspacial/nivel.h
+++ CarreraEspacial/nivel.h
@@ -1,40 +1,71 @@
 #ifndef NIVEL_H
 #define NIVEL_H
+
 #include <QGraphicsScene>
 #include <QGraphicsPixmapItem>
 #include <QGraphicsTextItem>
 #include <QTimer>
 #include <QVector>
+
 #include "jugador.h"
-#include "personaje.h"
+
 class Enemigo;
-class Nivel:public QGraphicsScene{
+
+class Nivel : public QGraphicsScene
+{
     Q_OBJECT
+
 public:
-    explicit Nivel(int numeroNivel,QObject *parent=nullptr);
-    QGraphicsPixmapItem *cohete()const{return m_cohete;}
-    void moverCohete(int dx,int dy);
+    explicit Nivel(int numeroNivel, QObject *parent = nullptr);
+
+    QGraphicsPixmapItem *cohete() const { return m_cohete; }
+    void moverCohete(int dx, int dy);
+
+    QGraphicsPixmapItem *mira() const { return m_mira; }
+    void moverMira(int dx, int dy);
+    void disparar();
+
 signals:
     void nivelCompletado(int numeroNivel);
     void nivelFallado(int numeroNivel);
+
 private slots:
     void actualizarCuentaAtras();
     void manejarCoheteRecolectado(QGraphicsItem *cohete);
     void onJugadorAtrapado();
+
+    void actualizarAliensNivel3();
+
 private:
     int m_numeroNivel;
+
     void crearNivel1();
     void crearNivel2();
     void crearNivel3();
-    Enemigo *m_enemigo=nullptr;
-    Jugador *m_jugador=nullptr;
+
+    void crearOleadaAliens();
+
+    Enemigo *m_enemigo = nullptr;
+    Jugador *m_jugador = nullptr;
     QVector<QGraphicsItem*> m_cohetes;
-    int m_cohetesRecolectados=0;
-    int m_cohetesTotales=0;
-    QTimer *m_timerNivel=nullptr;
-    int m_segundosRestantes=40;
-    QGraphicsTextItem *m_textoTiempo=nullptr;
-    QGraphicsTextItem *m_textoCohetes=nullptr;
-    QGraphicsPixmapItem *m_cohete=nullptr;
+    int m_cohetesRecolectados = 0;
+    int m_cohetesTotales      = 0;
+    QTimer *m_timerNivel      = nullptr;
+    int m_segundosRestantes   = 40;
+    QGraphicsTextItem *m_textoTiempo  = nullptr;
+    QGraphicsTextItem *m_textoCohetes = nullptr;
+
+    QGraphicsPixmapItem *m_cohete = nullptr;
+
+    QGraphicsPixmapItem *m_mira = nullptr;
+    QVector<Enemigo*>   m_aliensNivel3;
+
+    QTimer *m_timerAliensNivel3   = nullptr;
+    int     m_aliensEscapados     = 0;
+    int     m_aliensEscapadosMax  = 5;
+    int     m_oleadaActual        = 0;
+    int     m_totalOleadasNivel3  = 5;
+    int     m_aliensPorOleada     = 3;
 };
-#endif
+
+#endif // NIVEL_H
